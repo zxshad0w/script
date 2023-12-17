de15ee8c69d879f17af51e3063bcf8dc86a19678
arakul <143930010+zxshad0w@users.noreply.github.com>
2023-12-17 10:26:08 +0300

    zxc
    zxc

---
+++ Scripts.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //для выведеня в консоль
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
